9a8a49d93dfe1c781ea619baa4a96d0d20917aa6
Thalys Márcio <thalys@live.com>
2024-03-26 14:27:51 -0300

    Controlador

--- public/index.php
+++ public/index.php
@@ -1,27 +1,14 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require '../vendor/autoload.php';
 
+include '../app/Controllers/CarroController.php';
+
+use App\Controllers\CarroController;
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = AppFactory::create();
 
-/**
- * @OA\Info(title="API Locação", version="0.1")
- */
-
-
-/**
- * @SWG\Get(
- *     path="/carros",
- *     summary="Lista todos os carros",
- *     @SWG\Response(response=200, description="Sucesso")
- * )
- */
-$app->get('/carros', function (Request $request, Response $response, $args) {
-    return $response;
-});
+$app->get('/lista', [CarroController::class, 'lista']);
 
 $app->run();
