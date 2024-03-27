3bc9be90cfafc26f12362fe14cefbc16490ea4cb
Thalys Márcio <thalys@live.com>
2024-03-27 08:33:31 -0300

    API Endpoint

--- infrastructure/routes.php
+++ infrastructure/routes.php
@@ -7,9 +7,11 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-        $app->group('/carros', new Group(static function (RouteCollectorProxy $group): void {
+    $app->group('/api', function (RouteCollectorProxy $group) {
+        $group->group('/carros', function (RouteCollectorProxy $group) {
             $group->get('/lista', [CarroController::class, 'lista']);
-        }));
+        });
+    });
 
     $app->get('/swagger', function ($request, $response) use ($app) {
         $swaggerJson = file_get_contents(__DIR__ . '/swagger.json');
