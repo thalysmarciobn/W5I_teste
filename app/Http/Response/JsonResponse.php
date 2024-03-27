01a1c34c045c6a69b7319fd809f0c424c3774975
Thalys Márcio <thalys@live.com>
2024-03-26 22:16:26 -0300

    JSON Fix

--- app/Http/Response/JsonResponse.php
+++ app/Http/Response/JsonResponse.php
@@ -1,13 +1,14 @@
 <?php
 namespace App\Http\Response;
 
+use JMS\Serializer\SerializerBuilder;
 use Nyholm\Psr7\Response;
 
 final class JsonResponse extends Response
 {
     public function __construct(mixed $data, int $status = 200)
     {
-        $serializer = \JMS\Serializer\SerializerBuilder::create()->build();
+        $serializer = SerializerBuilder::create()->build();
         parent::__construct(
             $status,
             (['Content-Type' => 'application/json']),
